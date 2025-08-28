aa91aafdfc43bbdab73f8298015cee3bef8605af
Samruddhi <patilsamruddhi42@gmail.com>
2025-08-28 17:47:17 +0530

    header with cart logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import { useContext, useState } from "react";
-import { logo_URL } from "../utils/constants";
+import { CART_URL, logo_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContexts from "../utils/UserContexts";
 import { useSelector } from "react-redux";
-
 const Header = () => {
   const[btnNameReact,setBtnNameReact]= useState("LogIn")
   const onlineStatus = useOnlineStatus();
@@ -14,21 +13,21 @@ const Header = () => {
  const cartItems = useSelector((store) => store.cart.items);
   return (
     <div className="flex items-center p-2 shadow-md bg-green-50">
-      <div className="w-[130px] items-center shadow-md bg-green-50">
+      <div className="w-[130px] items-center shadow-md rounded-full  ">
         <img
-          className="logo"
+          className="logo rounded-full "
           src={logo_URL}
         />
       </div>
 
       <div className="ml-auto ">
-        <ul className="flex gap-5 items-center text-lg cursor-pointer mr-2">
-          <li>onlineStatus: {onlineStatus? "✅":"🔴"}</li>
-          <li> <Link to={"/"}> Home</Link></li>
-          <li><Link to={"/about"}> About Us </Link>  </li>
-          <li><Link to={"/contact"}> Contact Us</Link> </li>
-          <li><Link to={"/grocery"}> Grocery</Link> </li>
-          <li><Link to={"/cart"}> Cart ({cartItems.length} items)</Link></li>
+        <ul className="flex gap-5 items-center text-lg cursor-pointer mr-2 font-bold ">
+          <li className="hover:text-orange-600">onlineStatus: {onlineStatus? "✅":"🔴"}</li>
+          <li className="hover:text-orange-600"> <Link to={"/"}> Home</Link></li>
+          <li className="hover:text-orange-600"><Link to={"/about"}> About Us </Link>  </li>
+          <li className="hover:text-orange-600"><Link to={"/contact"}> Contact Us</Link> </li>
+          <li className="hover:text-orange-600"><Link to={"/grocery"}> Grocery</Link> </li>
+          <li className="hover:text-orange-600"><Link to={"/cart"} className="flex items-center"> <img src={CART_URL} alt="cart" className="h-[60px] w-15"/>({cartItems.length} items)</Link></li>
             
           <button className="px-5 py-1 mr-3 bg-orange-400 text-white rounded-lg"
           onClick={()=>{
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,9 @@ export const comman_URL =
 "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const logo_URL = 
-"https://static.vecteezy.com/system/resources/thumbnails/008/687/818/small/food-delivery-logo-free-vector.jpg"
+ " https://logowik.com/content/uploads/images/restaurant9491.logowik.com.webp"
 
 export const MENU_API = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=16.706369&lng=74.2481772&restaurantId=";
+
+export const CART_URL = 
+" https://www.reshot.com/preview-assets/icons/BAE3K9JRS7/cart-BAE3K9JRS7.svg";
